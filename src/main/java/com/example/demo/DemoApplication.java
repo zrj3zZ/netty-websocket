8526a141d62dfb32f61b2c1ccbd89174891d39ee
zrj3zZ <1289336748@qq.com>
2019-12-07 13:18:05 +0800

    zrj

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,8 +11,6 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
         try {
             new NettyServer(12345).start();
-            System.out.println("https://blog.csdn.net/moshowgame");
-            System.out.println("http://127.0.0.1:6688/netty-websocket/index");
         }catch(Exception e) {
             System.out.println("NettyServerError:"+e.getMessage());
         }
